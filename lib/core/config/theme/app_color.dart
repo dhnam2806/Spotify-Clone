a9c69a798c690f303275e6e8d2c9ca8bbf9938fa
DuongNam <20010913@st.phenikaa-uni.edu.vn>
2024-09-23 00:01:59 +0700

    first commit

---
+++ lib/core/config/theme/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xFF62CD5D);
+  static const background = Color(0xFFF2F2F2);
+}
+
+class AppColorsDark {
+
+  static const primary = Color(0xFF62CD5D);
+  static const background = Color(0xFF0D0C0C);
+}
